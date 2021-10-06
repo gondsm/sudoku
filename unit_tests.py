b87f717f3761b24e7fb4f2147b0038384a69b764
Gonçalo S. Martins <gondsm@gmail.com>
2021-10-06 11:42:51 +0100

    Re-work unit tests to use the unittest framework.

--- unit_tests.py
+++ unit_tests.py
@@ -1,14 +1,11 @@
 import copy
+import unittest
 
 # Local stuff:
 from game_board import GameBoard
 import solvers
 
-# TODO: re-write using unittest
-
-def test():
-    """ A few test cases to make sure things are working
-    """
+class TestStringMethods(unittest.TestCase):
     # A constant valid board. I absolutely did not copy this by hand from an example online.
     valid_board = [
         [5, 3, 4, 6, 7, 8, 9, 1, 2],
@@ -22,86 +19,7 @@ def test():
         [3, 4, 5, 2, 8, 6, 1, 7, 9],
     ]
 
-    print("Unit tests:")
-
-    # Sneak initialise with a valid board
-    board = GameBoard()
-    board._board = copy.deepcopy(valid_board)
-    print("Solved board working:")
-    print(board.is_solved() == True)
-
-    # Repeated value
-    board.value(0, 0, 2)
-    print("Repeated value working:")
-    print(board.is_valid() == False)
-
-    # Out-of-range value
-    board._board = copy.deepcopy(valid_board)
-    board.value(1, 1, 10)
-    print("Out-of-range value working:")
-    print(board.is_valid() == False)
-
-    # Incomplete board
-    board._board = copy.deepcopy(valid_board)
-    board.erase(0, 0)
-    print("Incomplete board working:")
-    print(board.is_complete() == False)
-
-    # Generate a random board and print it
-    print()
-    print("Random board:")
-    board.random_init()
-    print(board.printable())
-
-    print("Is this random board solved?")
-    print(board.is_solved())
-
-    print("Is this random board valid?")
-    print(board.is_valid())
-
-    print("Is this random board complete?")
-    print(board.is_complete())
-
-    # Modify the random board and print it
-    print()
-    board.erase(0, 0)
-    board.erase(0, 3)
-    board.erase(4, 2)
-    board.erase(8, 6)
-    print("Modified random board:")
-    print(board.printable())
-
-    print("Is this random board solved?")
-    print(board.is_solved())
-
-    print("Is this random board valid?")
-    print(board.is_valid())
-
-    print("Is this random board complete?")
-    print(board.is_complete())
-
-    # Random board iterator:
-    print()
-    elements = list(board.random_board_iterator())
-    print("Here's a random set of coordinates to access the board:")
-    print(elements)
-
-    print("Is this list duplicate-free?")
-    print(len(elements) == len(set((tuple(elem) for elem in elements))))
-
-    print("Does this list contain the expected number of elements (9x9 = 81)?")
-    print(len(elements) == 81)
-
-    # Test generating a board by solving an empty board
-    print("Generating a board...")
-    generated_board = solvers.backtrack()
-    print("Here's a generated board:")
-    print(generated_board.printable())
-
-    print("Is this board solved? It should be.")
-    print(generated_board.is_solved())
-    
-    # Solving a partial board
+    # The same board with a few elements removed, ripe for solving.
     partial_board = [
         [5, None, 4, 6, 7, 8, 9, 1, 2],
         [6, 7, None, 1, 9, 5, None, 4, 8],
@@ -114,19 +32,82 @@ def test():
         [3, 4, 5, 2, 8, 6, 1, 7, 9],
     ]
 
-    # Sneak in the board and solve it
-    board._board = copy.deepcopy(partial_board)
+    def test_valid_board(self):
+        """ A simple, complete and valid board should be considered solved.
+        """
+        board = GameBoard()
+        board._board = copy.deepcopy(self.valid_board)
 
-    print("Here's an incomplete board:")
-    print(board.printable())
+        self.assertTrue(board.is_solved())
 
-    solved_board = solvers.backtrack(board)
-    
-    print("Here's the solved board:")
-    print(solved_board.printable())
+    def test_repeated_value(self):
+        """ A board with a repeated value should not be valid.
+        """
+        board = GameBoard()
+        board._board = copy.deepcopy(self.valid_board)
+        board.value(0, 0, 2)
 
-    print("Is this board solved? It should be.")
-    print(solved_board.is_solved())
+        self.assertFalse(board.is_valid())
+
+    def test_out_of_range(self):
+        """ A board with an out-of-range value should not be valid.
+        """
+        board = GameBoard()
+        board._board = copy.deepcopy(self.valid_board)
+        board.value(1, 1, 10)
+
+        self.assertFalse(board.is_valid())
+
+    def test_incomplete(self):
+        """ A board that is missing an element should not be considered complete.
+        """
+        board = GameBoard()
+        board._board = copy.deepcopy(self.valid_board)
+        board.erase(0, 0)
+
+        self.assertFalse(board.is_complete())
+
+    def test_random_board(self):
+        """ Test generating a random board.
+
+        A random board should (extremely likely) not be solved, should (extremely likely) not be valid, and should be
+        complete.
+        """
+        board = GameBoard()
+        board.random_init()
+
+        self.assertFalse(board.is_solved())
+        self.assertFalse(board.is_valid())
+        self.assertTrue(board.is_complete())
+
+    def test_random_board_iterator(self):
+        """ Test generating a random board iterator.
+        """
+        board = GameBoard()
+        elements = list(board.random_board_iterator())
+
+        self.assertTrue(len(elements) == len(set((tuple(elem) for elem in elements))))
+        self.assertTrue(len(elements) == 81)
+
+    def test_generate_from_empty(self):
+        """ If we solve an empty board, it should generate a solved board.
+        """
+        generated_board = solvers.backtrack()
+        self.assertTrue(generated_board.is_solved())
+
+    def test_solve_from_partial(self):
+        """ If we solve a partial board, it should generated a solved board.
+
+        The solved board should be equal to the valid board, since I didn't remove enough elements to generate a
+        multi-solution problem.
+        """
+        board = GameBoard()
+        board._board = copy.deepcopy(self.partial_board)
+        solved_board = solvers.backtrack(board)
+
+        self.assertTrue(solved_board.is_solved)
+        self.assertEqual(solved_board._board, self.valid_board)
+
 
 if __name__ == "__main__":
-    test()
+    unittest.main()
